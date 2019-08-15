777bdb3f941aa9db9d6234db68509f942a917820
Robert Keffury <bobby.keffury@me.com>
2019-08-15 10:52:35 -0700

    Created ThemeSelectionViewController scene and it's corresponding file, buttons, and label.

---
+++ PhotoCollectionProject/PhotoCollectionProject/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollectionProject
+//
+//  Created by Bobby Keffury on 8/15/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    var themeHelper: ThemeHelper?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func selectDarkThemeTapped(_ sender: Any) {
+    }
+    
+    @IBAction func selectLightThemeTapped(_ sender: Any) {
+    }
+    
+    
+}
