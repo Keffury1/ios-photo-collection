ccbf4e132f09093b94977bf4d5470738457ac141
Robert Keffury <bobby.keffury@me.com>
2019-08-15 10:40:43 -0700

    Created PhotoViewController scene and added PhotoDetailViewController file. Added 2 segues.

---
+++ PhotoCollectionProject/PhotoCollectionProject/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollectionProject
+//
+//  Created by Bobby Keffury on 8/15/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    var photoController: PhotoController?
+    
+    var photo: Photo?
+    
+    var themeHelper: ThemeHelper?
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var photoTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func addPhotoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+}
