6c584eea2243568043550562ad88906c9f59125a
Robert Keffury <bobby.keffury@me.com>
2019-08-15 09:36:21 -0700

    Finished Photo file and struct

---
+++ PhotoCollectionProject/PhotoCollectionProject/Model/Photo.swift
@@ -0,0 +1,17 @@
+//
+//  Photo.swift
+//  PhotoCollectionProject
+//
+//  Created by Bobby Keffury on 8/15/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+    
+}
+
---
+++ PhotoCollectionProject/PhotoCollectionProject/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollectionProject
+//
+//  Created by Bobby Keffury on 8/15/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
